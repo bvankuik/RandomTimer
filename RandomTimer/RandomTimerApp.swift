b69c0cd2497d1de911e0f713575fe1574e4faea2
Bart van Kuik <bart@dutchvirtual.nl>
2021-02-13 17:09:26 +0100

    Initial Commit

---
+++ RandomTimer/RandomTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomTimerApp.swift
+//  RandomTimer
+//
+//  Created by Bart van Kuik on 13/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct RandomTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
