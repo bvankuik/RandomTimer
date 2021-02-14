3d709cdc2a937d71b1c018ec21ecc24305ed1f42
Bart van Kuik <bart@dutchvirtual.nl>
2021-02-14 19:04:55 +0100

    Steppers and input fields for range

--- RandomTimer/ContentView.swift
+++ RandomTimer/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number: Int = 0
+    @State private var done: [Int] = []
+    @AppStorage("startRange") private var startRange = 1
+    @AppStorage("endRange") private var endRange = 30
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                TextField("Start of range", text: self.makeIntBinding(to: self.$startRange))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Stepper(value: self.$startRange, label: {})
+            }
+            HStack {
+                TextField("End of range", text: self.makeIntBinding(to: self.$endRange))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Stepper(value: self.$endRange, label: {})
+            }
+            Spacer()
+            Text("\(number)").font(Font.system(size: 120))
+            Spacer()
+            Button(action: buttonAction) {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .foregroundColor(Color(UIColor.secondarySystemFill))
+                    .frame(maxHeight: 120)
+                    .overlay(Text("New Number").font(.title).foregroundColor(.primary))
+            }
+        }
+        .padding(50)
+    }
+    
+    private func buttonAction() {
+        self.done.append(self.number)
+        self.number = self.newRandomNumber()
+    }
+    
+    private func makeIntBinding(to intState: Binding<Int>) -> Binding<String> {
+        Binding<String>(
+            get: {
+                return String(describing: intState.wrappedValue)
+            },
+            set: { newValue in
+                if let newInt = Int(newValue) {
+                    intState.wrappedValue = newInt
+                }
+            }
+        )
+    }
+
+    private func newRandomNumber() -> Int {
+        var new = 0
+        while new == 0 {
+            if let randomElement = (1 ... 10).randomElement() {
+                new = randomElement
+            }
+        }
+        return new
     }
 }
 
